21da7a75a0023c954f1a5c568a0468f4c2202f6f
Resspectine <resspectyn@gmail.com>
2016-11-13 00:57:07 +0300

    Разобрался с позиционированием

--- src/HOLDER/Frame.java
+++ src/HOLDER/Frame.java
@@ -2,22 +2,29 @@ package HOLDER;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Frame {
     public static void createGUI() {
         JFrame frame = new JFrame("Есть контакт");
         frame.setLocation(500 ,100);
-        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setPreferredSize(new Dimension(600, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
-        JLabel label = new JLabel("Test label");
-        label.setPreferredSize(new Dimension(100,20));
-        panel.add(label);
+        panel.setLayout(null);
+        JLabel label1= new JLabel("Чатик:");
+        label1.setBounds(10,5,100,40);
+        panel.add(label1);
+        JFormattedTextField dialog = new JFormattedTextField();
+        dialog.setBounds(10,35,500,500);
+        panel.add(dialog);
         JButton button1= new JButton("Нажми");
-        button1.setSize(new Dimension(100,100));
-        button1.setLocation(200,100);
-        panel.add(button1);
-
+        button1.setBounds(250,100,200,100);
+        //panel.add(button1);
+        JButton button2= new JButton("Нажми2");
+        button2.setBounds(20,100,200,100);
+       // panel.add(button2);
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
