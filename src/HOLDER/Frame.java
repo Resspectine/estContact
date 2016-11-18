5785b83dc3fce3c8136677f39b2ef97be7681cf2
Resspectine <resspectyn@gmail.com>
2016-11-18 19:36:48 +0300

    Она работает

--- src/HOLDER/Frame.java
+++ src/HOLDER/Frame.java
@@ -13,27 +13,32 @@ public class Frame {
         frame.setLocation(500 ,100);
         frame.setPreferredSize(new Dimension(600, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         JPanel panel = new JPanel();
         panel.setLayout(null);
+
         JLabel label1= new JLabel("Чатик:");
         label1.setBounds(10,5,100,40);
         panel.add(label1);
+
         JFormattedTextField dialog = new JFormattedTextField();
         dialog.setBounds(10,35,200,200);
-
+        ActionListener action1= new ButtonListner();
         panel.add(dialog);
+
         JButton button= new JButton("Нажми");
         button.setBounds(220,215,100,20);
-        button.setActionCommand("Button1");
+        button.addActionListener(action1);
         panel.add(button);
+
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
     }
 }
-class ButtonListner implements ActionListener{
+class ButtonListner implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-
+            System.out.println("Работает");
     }
 }
